c95a3b3ec878e9358e04392ca0d9f2a5942a64ea
Stark <serj.elessar@gmail.com>
2022-07-26 20:49:31 +0300

    Added the ability to send data from the form to the mail

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { useInput } from './hooks/useInput'
+import emailjs from '@emailjs/browser'
 
 const App = () => {
   const [theme, setTheme] = useState(false)
   const email = useInput('', { isEmpty: true, isMail: true })
   const password = useInput('', { isEmpty: true, minLength: 6, maxLength: 12 })
+  const phone = useInput('', { isEmpty: true, minLength: 9, maxLength: 13 })
 
   const handleSetTheme = () => setTheme(!theme)
   const darkTheme = theme ? 'dark' : ''
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_4b02ni1', 'template_4w6byi5', form.current, '3M80LCpJrLNA6Po3z')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
 
   return (
     <div className={`wrapper ${darkTheme}`}>
@@ -22,53 +37,71 @@ const App = () => {
         <h2 className={`form__sub-title title ${darkTheme}`}>
           Sign in and start managing your candidates!
         </h2>
-        <div className="form__input-block">
-          {(email.isDirty && email.isEmpty) && <div className='form__input-error'>{email.emptyFields}</div>}
-          {(email.isDirty && email.emailError) && <div className='form__input-error'>{email.invalidEmail}</div>}
-          <input
-            value={email.value}
-            onChange={(e) => email.onChange(e)}
-            onBlur={email.onBlur}
-            type='text'
-            name='email'
-            placeholder='Введите ваш E-mail..'
-            className={`form__input ${darkTheme}`}
-          />
-        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <div className="form__input-block">
+            {(email.isDirty && email.isEmpty) && <div className='form__input-error'>{email.emptyFields}</div>}
+            {(email.isDirty && email.emailError) && <div className='form__input-error'>{email.invalidEmail}</div>}
+            <input
+              value={email.value}
+              onChange={(e) => email.onChange(e)}
+              onBlur={email.onBlur}
+              type='text'
+              name='email'
+              placeholder='Введите ваш E-mail..'
+              className={`form__input ${darkTheme}`}
+            />
+          </div>
 
-        <div className="form__input-block">
-          {(password.isDirty && password.isEmpty) && <div className='form__input-error'>{email.emptyFields}</div>}
-          {(password.isDirty && password.minLengthError) && <div className='form__input-error'>{email.shortPass}</div>}
-          {(password.isDirty && password.maxLengthError) && <div className='form__input-error'>{email.longPass}</div>}
-          <input
-            value={password.value}
-            onChange={(e) => password.onChange(e)}
-            onBlur={password.onBlur}
-            type='password'
-            name='password'
-            placeholder='Введите ваш пароль..'
-            className={`form__input ${darkTheme}`}
-          />
-        </div>
+          <div className="form__input-block">
+            {(password.isDirty && password.isEmpty) && <div className='form__input-error'>{email.emptyFields}</div>}
+            {(password.isDirty && password.minLengthError) && <div className='form__input-error'>{email.shortPass}</div>}
+            {(password.isDirty && password.maxLengthError) && <div className='form__input-error'>{email.longPass}</div>}
+            <input
+              value={password.value}
+              onChange={(e) => password.onChange(e)}
+              onBlur={password.onBlur}
+              type='password'
+              name='password'
+              placeholder='Введите ваш пароль..'
+              className={`form__input ${darkTheme}`}
+            />
+          </div>
 
-        <div className='form__row row'>
-          <label className={`form__label ${darkTheme}`}>
-            <input type='checkbox' name='remember' />
-            <span className={`form__label-check ${darkTheme}`}></span>
-            <span className={`form__label-text ${darkTheme}`}>Remember Me</span>
-          </label>
+          <div className="form__input-block">
+            {(phone.isDirty && phone.isEmpty) && <div className='form__input-error'>{phone.emptyFields}</div>}
+            {(phone.isDirty && phone.minLengthError) && <div className='form__input-error'>{phone.shortTel}</div>}
+            {(phone.isDirty && phone.maxLengthError) && <div className='form__input-error'>{phone.longTel}</div>}
+            <input
+              value={phone.value}
+              type="tel"
+              onChange={(e) => phone.onChange(e)}
+              onBlur={phone.onBlur}
+              className={`form__input ${darkTheme}`}
+              placeholder='Введите ваш номер телефона..'
+              name='user_phone'
+            />
+          </div>
 
-          <span className={`form__forgot ${darkTheme}`} >
-            Forgot Password?
-          </span>
-        </div>
+          <div className='form__row row'>
+            <label className={`form__label ${darkTheme}`}>
+              <input type='checkbox' name='remember' />
+              <span className={`form__label-check ${darkTheme}`}></span>
+              <span className={`form__label-text ${darkTheme}`}>Remember Me</span>
+            </label>
 
-        <button
-          disabled={!email.inputValid || !password.inputValid}
-          className={`form__button-login ${darkTheme}`}
-        >
-          Login
-        </button>
+            <span className={`form__forgot ${darkTheme}`} >
+              Forgot Password?
+            </span>
+          </div>
+
+          <button
+            disabled={!email.inputValid || !password.inputValid}
+            className={`form__button-login ${darkTheme}`}
+            type='submit'
+          >
+            Login
+          </button>
+        </form>
       </div>
     </div>
   )
